2ea8a14659a00515a62cdf44d37a7b39c234e598
carouzinha <123772397+carouzinha@users.noreply.github.com>
2023-05-19 11:50:50 -0300

    Primeira aula de Python

---
+++ print.py
@@ -0,0 +1,28 @@
+print("Mensagem que eu quero")
+
+print ('Mensagem')
+
+print('Fulano é meu "amigo"')
+
+print("Fulano \n é meu \"amigo\"")
+
+print(1+1)
+
+print("1 x 1=" ,10**3)
+
+print("10² =" ,pow(10,3))
+
+nome = ""
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+print(nome)
+
+
+
+
+
+
